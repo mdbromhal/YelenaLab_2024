8e60831cda4a771d8ba39fa8c71bc76adce4fd0f
mdbromhal <mdalia@proton.me>
2024-02-13 09:06:09 -0500

    "added green_detect file"

---
+++ green_detect.py
@@ -0,0 +1,26 @@
+# 13 Feb. 2023
+# Megdalia Bromhal
+# References: https://pysource.com/2019/02/15/detecting-colors-hsv-color-space-opencv-with-python/
+
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # Green color
+    low_green = np.array([25, 52, 72])
+    high_green = np.array([102, 255, 255])
+    green_mask = cv2.inRange(hsv_frame, low_green, high_green)
+    green = cv2.bitwise_and(frame, frame, mask=green_mask)
+
+    # Show object with green color
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Green", green)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
