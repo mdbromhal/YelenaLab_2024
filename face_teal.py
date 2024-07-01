660c327ff63f2174bb45e599e05aed3bb483b5d5
mdbromhal <mdalia@proton.me>
2024-07-01 13:21:44 -0400

    added sonar from avoid.py from Sunfounder to use

--- face_teal.py
+++ face_teal.py
@@ -139,6 +139,51 @@ def main():
                     elif angle <= cbuff:
                         print("Object in center buffer")
                         
+                        # If teal is on a wall in front of Yelena
+                        
+                        # From avoid.py by Sunfounder
+#                         from picrawler import Picrawler
+#                         from robot_hat import TTS, Music
+#                         from robot_hat import Ultrasonic
+#                         from robot_hat import Pin
+#                         import time
+#                         import os
+# 
+#                         tts = TTS()
+#                         music = Music()
+# 
+#                         crawler = Picrawler([10,11,12,4,5,6,1,2,3,7,8,9]) 
+#                         #crawler.set_offset([0,0,0,0,0,0,0,0,0,0,0,0])
+#                         sonar = Ultrasonic(Pin("D2") ,Pin("D3"))
+# 
+#                         alert_distance = 15
+# 
+#                         speed = 100
+# 
+#                         def main():
+#                             distance = sonar.read()
+#                             print(distance)
+#                             if distance < 0:
+#                                 pass
+#                             elif distance <= alert_distance:
+#                                 try:
+#                                     music.sound_effect_threading('./sounds/sign.wav')
+#                                 except Exception as e:
+#                                     print(e)
+#                                 crawler.do_action('turn left angle',3,speed)
+#                                 time.sleep(0.2)
+#                             else :
+#                                 crawler.do_action('forward', 1,speed)
+#                                 time.sleep(0.2)
+                        sonar = Ultrasonic(Pin("D2"), Pin("D3"))
+                        
+                        alert_distance = 15
+                        
+                        distance = sonar.read()
+                        print(distance)
+                        
+                        if distance <= alert_distance
+                        
                         # Using Sunfounder's code to move Yelena forward
                         yelena_move.move_forward(speed=70, crawler=crawler)
                         
